5c6602b8d823a10330916604a4bc1c446c288556
Logan Born <logan.o.born@gmail.com>
2019-11-14 13:55:11 -0800

    Added basic paging to the tablet previews

--- tablets.js
+++ tablets.js
@@ -1,38 +1,40 @@
 var transliterations;
 
-function grep_tablets( selection ) {
-  console.log("enter grep");
-  var search_str = selection.left.split(" ");
-  search_str = search_str.slice(0,search_str.length-(document.getElementById("center_sign").value.split(" ").length)).join(" ");
-  search_str += " "+selection.right;
-  search_str = search_str.replace("x","+").replace("lpar","(").replace("rpar",")").replace("unk","[...]").replace("x","+");
-  while (/( |^)([^ |]*\+[^| ]*)( |$)/.test(search_str)) {
-    console.log("replace",search_str);
-    search_str = search_str.replace(/( |^)([^ |]*\+[^| ]*)( |$)/,
-      function(match,p1,p2,p3){return " |"+p2+"| "} 
-    )
-    console.log("replaced",search_str);
-  }
-  console.log("untrim",search_str);
-  search_str = search_str.trim();
-  console.log(search_str);
+var pages = [];
+var search_str_saved = "";
+var current_page = 0;
 
-  var tablets_with_string = [];
-  for ( var i = 0; i < transliterations.length; i++ ) {
-    if ( transliterations[i].includes(search_str) ) {
-      //console.log( transliterations[i] );
-      tablets_with_string.push( transliterations[i].split(" ") );
-      if ( tablets_with_string.length >= 5) {
-	break;
-      }
-    }
-  }
+function show_page( n ) {
+  current_page = n;
 
-  tablets_with_string = tablets_with_string.slice(0,5);
+  $( "#tablet_previews" ).html( '' );
+
+  var buttons = document.createElement("div");
+  //buttons.className = "tablet_preview";
+  buttons.id = "preview_buttons";
+  buttons.style= "text-align:center;";
+  var nextPg = document.createElement("a");
+  nextPg.href="javascript:void(0);";
+  nextPg.innerHTML = "&nbsp;&gt; next page&nbsp;";
+  nextPg.onclick = function(){show_page(current_page+1);};
+  var lastPg = document.createElement("a");
+  lastPg.href="javascript:void(0);";
+  lastPg.innerHTML = "&nbsp;prev page &lt;&nbsp;";
+  lastPg.onclick = function(){show_page(current_page-1);};
+  if ( current_page > 0 ) {
+    buttons.appendChild( lastPg );
+  }
+  if ( current_page < Math.ceil(pages.length/5)-1 ) {
+    buttons.appendChild( nextPg );
+  }
+  $( "#tablet_previews" ).append( buttons );
+
+
+  var search_str = search_str_saved;
+  var tablets_with_string = pages.slice(5*n,5*(1+n));
   var hilites = myDiff( tablets_with_string, search_str );
   //console.log(hilites);
 
-  $( "#tablet_previews" ).html( '' );
   for ( var i = 0; i < tablets_with_string.length; i++ ) {
     var hilite = false;
     var color = "";
@@ -53,6 +55,40 @@ function grep_tablets( selection ) {
   }
 }
 
+function grep_tablets( selection ) {
+  //console.log("enter grep");
+  var search_str = selection.left.split(" ");
+  search_str = search_str.slice(0,search_str.length-(document.getElementById("center_sign").value.split(" ").length)).join(" ");
+  search_str += " "+selection.right;
+  search_str = search_str.replace("x","+").replace("lpar","(").replace("rpar",")").replace("unk","[...]").replace("x","+");
+  while (/( |^)([^ |]*\+[^| ]*)( |$)/.test(search_str)) {
+    //console.log("replace",search_str);
+    search_str = search_str.replace(/( |^)([^ |]*\+[^| ]*)( |$)/,
+      function(match,p1,p2,p3){return " |"+p2+"| "} 
+    )
+    //console.log("replaced",search_str);
+  }
+  //console.log("untrim",search_str);
+  search_str = search_str.trim();
+  console.log(search_str);
+
+  var tablets_with_string = [];
+  for ( var i = 0; i < transliterations.length; i++ ) {
+    // add space to account for variants
+    if ( (transliterations[i]+" ").includes(search_str+" ") ) {
+      //console.log( transliterations[i] );
+      tablets_with_string.push( transliterations[i].split(" ") );
+      /*if ( tablets_with_string.length >= 5) {
+	break;
+      }*/
+    }
+  }
+  pages = tablets_with_string;
+  search_str_saved = search_str;
+  show_page(0);
+
+}
+
 function myDiff( files, search_str ) {
   var open  = new Object();
   var pastFinds = new Object();
@@ -60,12 +96,12 @@ function myDiff( files, search_str ) {
   // highlight the selected signs
   for ( var i = 0; i < files.length; i++ ) {
         for ( var j = i+1; j < files.length; j++ ) {
-    for (var len = 5; len > 2; len-- ) {
+    for (var len = 5; len > 1; len-- ) {
       for ( var start = 0; start < files[i].length-len+1; start++ ){
         var str = files[i].slice(start,start+len).join(" ");
-	//if (str.includes("N")){
-	//  continue;
-	//}
+	  if (str.includes("N")){
+	    continue;
+	  }
           //for (var len2 = 3; len2 < 4; len2++ ) {
 	  var len2 = len;
             for ( var start2 = 0; start2 < files[j].length+1-len2; start2++ ){
